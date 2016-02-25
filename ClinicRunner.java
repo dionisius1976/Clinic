1d22a374cda2a77275ecb78cb9355f1f8ef64806
Dionisius <maslo111@rambler.ru>
2016-02-25 17:40:40 +0300

    Added files via upload

--- ClinicRunner.java
+++ ClinicRunner.java
@@ -10,32 +10,34 @@ public class ClinicRunner{
 		
 		final Clinic clinic = new Clinic(10);
 		Scanner reader = new Scanner(System.in);
-		String quit = "no";
+		Boolean quit = false;
+		String choice;
 		
-		while(!quit.equals("yes")){
+		while(!quit){
 		
 			System.out.println("Choice your action:");
-			System.out.println("Add new client: 				(1) ");
-			System.out.println("Find client by pet's name: 		(2) ");
-			System.out.println("Find pet by client's name: 		(3) ");
-			System.out.println("Edit client's name:		 		(4) ");
-			System.out.println("Edit pet's name: 				(5) ");
-			System.out.println("Delete client by his name: 		(6) ");
-			System.out.println("Delete client by pet's name: 	(7) ");
-			System.out.println("Print all clients:				(8) ");
-			
-			clinic.operationChoice(reader.next());
-			
-				
-			System.out.print("Quit? (yes/no): ");
-			quit = reader.next();
 			System.out.println();
-		
+			System.out.println("1 - Add new client;");
+			System.out.println("2 - Find client by pet's name;");
+			System.out.println("3 - Find pet by client's name;");
+			System.out.println("4 - Edit client's name;");
+			System.out.println("5 - Edit pet's name;");
+			System.out.println("6 - Delete client by his name;");
+			System.out.println("7 - Delete client by pet's name;");
+			System.out.println("8 - Print all clients;");
+			System.out.println("9 - Quit.");
+			
+			choice = reader.next();
+			
+			if ("9".equals(choice)) {quit = true; continue;}
+			
+			clinic.operationChoice(choice);
+			System.out.println();
+			
 		}
 	
 	reader.close();
 	System.out.print("Bye!");
 		
 	}
-
 }
